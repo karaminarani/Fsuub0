883c6e8c2bdb480268189e101178ec3f3ae2eb46
karami narani <157799613+karaminarani@users.noreply.github.com>
2024-04-12 23:02:35 +0700

    Update start.py

--- Bot/Plugins/start.py
+++ Bot/Plugins/start.py
@@ -14,11 +14,9 @@ async def Start(Bot, Msg):
     user_id = Msg.from_user.id
 
     BotStartMessage = "**Halo {mention} ​! Saya dapat menyimpan file pribadi di Channel tertentu dan pengguna lain dapat mengaksesnya dari link khusus..**"
-    MustJoinMessage = "**"Halo {mention} ! "
+    MustJoinMessage = "**""Hayolo Ketahuan Belum Join Channel dan Groupnya Yaa, Yuk Join Dulu Biar Bisa Buka Linknya."
     "\n\n"
-    "Hayolo Ketahuan Belum Join Channel dan Groupnya Yaa, Yuk Join Dulu Biar Bisa Buka Linknya."
-    "\n\n"
-    "Silakan Join Ke Channel dan Groupnya di Bawah Ini Terlebih Dahulu Yaa.**" 
+    "Silakan Join Ke Channel dan Groupnya di Bawah Ini Terlebih Dahulu Yaa..**"
 
     Bot.UserDB.Insert(user_id)
 
